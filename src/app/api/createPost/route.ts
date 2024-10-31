c319d827ef6697a17f9010392414ebc2e889fb44
dylan h <baolong281@gmail.com>
2024-10-31 10:28:49 -0500

    const

--- src/app/api/createPost/route.ts
+++ src/app/api/createPost/route.ts
@@ -1,6 +1,6 @@
 import { createPost } from "@/db/queries/insert";
 export async function POST(request: Request) {
   const data = await request.json();
-  let response = await createPost(data);
+  const response = await createPost(data);
   return Response.json(response);
 }
