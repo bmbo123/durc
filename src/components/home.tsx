09aecb49b61d9ad0a508d7e8cf47ff10aaf2d965
dylan h <baolong281@gmail.com>
2024-10-31 16:56:15 -0500

    rebranding

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -9,8 +9,10 @@ export default async function Home() {
   // but if we want to create / update data we need an api end point that will be called with fetch
   const posts = await getPosts();
   return (
-    <div className="h-screen bg-stone-950 pt-12 align-center flex flex-col justify-start gap-8 w-[600px]">
-      <h1 className="text-4xl font-bold text-white text-center">FEIN AI</h1>
+    <div className="h-screen bg-stone-950 pt-16 align-center flex flex-col justify-start gap-8 w-[600px]">
+      <h1 className="text-4xl font-bold text-center italic underline tracking-wider text-white underline-offset-4 decoration-indigo-500">
+        FEIN AI
+      </h1>
       <Tweet />
       <div>
         {posts?.map((post, i) => {
