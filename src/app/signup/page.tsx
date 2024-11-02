2d39d1b4c6bde08ae738bd4fb03be1a7d99070c2
bmbo123 <gregoryalan2005@gmail.com>
2024-11-02 11:40:05 -0500

    added sign up page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,36 @@
+"use client";
 import Home from "@/components/home";
-
+import { useState } from "react";
+import { ArrowRight } from "lucide-react";
 export default function Page() {
+  const [hover, sethover] = useState(false);
   return (
-    <div className="bg-stone-950 h-screen flex justify-center top">
-      <p className="font-sans text-5xl font-semibold ">Sign Up</p>
+    <div className="bg-stone h-screen flex flex-col items-center justify-center">
+      <p className="font-sans  font-semibold mb-4">
+        sign up or{" "}
+        <span className="hover:text-indigo-700 underline font-bold">
+          {" "}
+          sign in
+        </span>
+      </p>
+      <div className="flex flex-col gap-2">
+        <input
+          type="email"
+          placeholder="Enter your email"
+          className="border-l-2 border-gray-300  w-64 p-2 bg-stone-950 text-white focus:outline-none"
+        />
+        <input
+          type="password"
+          placeholder="Enter your password"
+          className="border-l-2 border-gray-300  w-64 p-2 bg-stone-950 text-white focus:outline-none "
+        />
+      </div>
+      <button
+        onMouseOver={() => sethover(true)}
+        onMouseLeave={() => sethover(false)}
+      >
+        <ArrowRight strokeWidth={hover ? 2 : 1} />
+      </button>
     </div>
   );
 }
