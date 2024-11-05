6c1d90d815d30cbb883595db0e3138d789f16d4b
dylan h <baolong281@gmail.com>
2024-11-04 22:50:38 -0600

    starting profile page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,8 @@ import Home from "@/components/home";
 
 export default function Page() {
   return (
-    <div className="bg-stone-950 h-screen flex align-middle justify-center relative">
+    <div className="bg-stone-950 h-screen flex align-middle justify-center ">
       <Home />
-      <div className="absolute top-0 right-0 p-4 flex flex-col"></div>
     </div>
   );
 }
